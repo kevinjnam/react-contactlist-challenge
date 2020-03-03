b3629076a50e402aeba8a7b7359e6d9f88cff02d
Kevin Nam <kevinjnam@gmail.com>
2020-03-03 14:56:04 -0800

    mvp without styling complete

--- components/Divider.js
+++ components/Divider.js
@@ -3,16 +3,7 @@ import React from 'react';
 export default function Divider({ category, children }) {
   return (
     <div>
-      <h3
-        style={{
-          display: 'flex',
-          fontSize: '1.5rem',
-          justifyContent: 'space-between',
-          borderBottom: '1px solid #eee'
-        }}
-      >
-        {category}
-      </h3>
+      <h3>{category}</h3>
       {children}
     </div>
   );
