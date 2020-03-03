f32b8cd17f2f4ed0e717473dc1b19a2dae763570
Kevin Nam <kevinjnam@gmail.com>
2020-03-03 09:02:05 -0800

    installed redux-devtools-extension as dev-dependency and set up redux dev tools

--- utils/configureStore.js
+++ utils/configureStore.js
@@ -1,9 +1,14 @@
-import { applyMiddleware, createStore } from "redux";
-import reduxThunk from "redux-thunk";
-import reducers from "../reducers";
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reduxThunk from 'redux-thunk';
+import reducers from '../reducers';
 
 export default function configureStore(initialState) {
   const middlewares = [reduxThunk];
 
-  return createStore(reducers, initialState, applyMiddleware(...middlewares));
+  return createStore(
+    reducers,
+    initialState,
+    composeWithDevTools(applyMiddleware(...middlewares))
+  );
 }
