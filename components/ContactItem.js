4bda099f26d0f87f69e1d1fe3570bffdcd667906
Kyle Lawson <Kyle.Lawson7@yahoo.com>
2020-03-02 18:02:13 -0600

    add redux updates

---
+++ components/ContactItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function ContactItem({ firstName, lastName, phone }) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        borderBottom: "1px solid #eee"
+      }}
+    >
+      <p>
+        {firstName} <strong>{lastName}</strong>
+      </p>
+      <p>{phone}</p>
+    </div>
+  );
+}
